f45af97bd30bcf9bd739f99c6b18239b0fc57aa2
Aleksandr Prokhorenkov <89263379173@yandex.ru>
2026-09-21 10:57:04 +0300

    Message Change

--- logistic/views.py
+++ logistic/views.py
@@ -24,4 +24,4 @@ class StockViewSet(ModelViewSet):
 
 @api_view(['GET'])
 def sample_view(request):
-    return Response({'message': 'Finish settings CI with deploy!!!'})
+    return Response({'message': 'Настройки CI CD закончены с деплоем!!!'})
